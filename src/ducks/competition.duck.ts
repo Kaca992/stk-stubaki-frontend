51527a76e09250021c03b2ded51fb0101e76f791
kaca992 <kacavendadino@gmail.com>
2018-01-25 20:08:54 +0100

    name change

---
+++ src/ducks/competition.duck.ts
@@ -0,0 +1,49 @@
+import { createSelector } from 'reselect';
+
+import { fetcher, actionUtils, ICustomFetchOptions } from '../utils/fetcher';
+import { IAction } from '../common/interfaces';
+import { IStore } from '../store/index';
+// action types
+
+const actionTypes = {
+
+};
+
+// action creators
+
+const actionCreators = {
+
+};
+
+// reducer
+
+export interface ICompetitionState {
+    UI: {
+        isLoading: false
+    }
+}
+
+const initialState: ICompetitionState = {
+    UI: {
+        isLoading: false
+    }
+};
+
+const reducer = (state= initialState, action: IAction): ICompetitionState => {
+    switch (action.type) {
+
+    }
+    return state;
+};
+
+// selectors
+const selectors = {
+
+};
+
+export const TablicaDuck = {
+    actionTypes,
+    actionCreators,
+    reducer,
+    selectors
+};
